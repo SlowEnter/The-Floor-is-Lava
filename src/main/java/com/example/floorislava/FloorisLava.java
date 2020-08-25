38130c24b1be576cd65482237fcd7653e73fd2f4
SlowEnter <ianhacha95@gmail.com>
2020-08-26 00:36:21 +0100

    Creating Generic structure, added main mod information

---
+++ src/main/java/com/example/floorislava/FloorisLava.java
@@ -0,0 +1,27 @@
+package com.example.floorislava;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("floorislava")
+public class FloorisLava {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
+    public FloorisLava() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) { }
+
+    private void doClientStuff(final FMLClientSetupEvent event) { }
+
+}
